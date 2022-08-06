d9a79861fa3546fa23d2e243514381653a09ce39
Charahiro-tan <73784718+Charahiro-tan@users.noreply.github.com>
2022-08-07 02:52:45 +0900

    ビルド用

---
+++ build.py
@@ -0,0 +1,37 @@
+import platform
+
+import PyInstaller.__main__
+
+OS = platform.system()
+
+if OS == 'Windows':
+    PyInstaller.__main__.run([
+        'twitchTransFN.py',
+        '--clean',
+        '--onefile',
+        '--hidden-import=pywin32',
+        '--runtime-tmpdir="./"',
+        '--icon=icon.ico',
+        '--exclude-module="config"',
+        '--name=twitchTransFN.exe'
+    ])
+elif OS == 'Darwin':
+    PyInstaller.__main__.run([
+        'twitchTransFN.py',
+        '--clean',
+        '--onefile',
+        '--runtime-tmpdir="./"',
+        '--icon=icon.ico',
+        '--exclude-module="config"',
+        '--name=twitchTransFN.command'
+    ])
+elif OS == 'Linux':
+    PyInstaller.__main__.run([
+        'twitchTransFN.py',
+        '--clean',
+        '--onefile',
+        '--runtime-tmpdir="./"',
+        '--icon=icon.ico',
+        '--exclude-module="config"',
+        '--name=twitchTransFN'
+    ])
